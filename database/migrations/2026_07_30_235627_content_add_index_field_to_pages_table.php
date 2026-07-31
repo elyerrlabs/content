9e6aa8cec32dd89669fe647527f2cbbb3d22e855
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-07-30 19:04:30 -0500

    Fix content_pages migration when index field already exists
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

---
+++ database/migrations/2026_07_30_235627_content_add_index_field_to_pages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasColumn('content_pages', 'index')) {
+            Schema::table('content_pages', function (Blueprint $table) {
+                $table->boolean('index')->default(false)->index();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasColumn('content_pages', 'index')) {
+            Schema::table('content_pages', function (Blueprint $table) {
+                $table->dropColumn('index');
+            });
+        }
+    }
+};
